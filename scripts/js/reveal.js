8ac07c56879f7261c4008f1dfe86fd6acb8e88e3
Zdeněk Tomis <zdnek.tomis@gmail.com>
2024-02-11 19:18:39 +0100

    update js

--- scripts/js/reveal.js
+++ scripts/js/reveal.js
@@ -1,3 +1,38 @@
-$(document).ready(function () {
-    $(".bidding-solution > not(:first-child)").css("background-color", "cyan");
+$(function () {
+    $(".bidding-solution").each( function(index, el) {
+
+        let fn =  function () {
+            $(el).children(":not(.solution-label):not(.reveal-button)").removeClass("blur");
+            $(el).children(".reveal-button").css("visibility", "hidden");
+        }
+        let button = $("<button>").append($("<i>").addClass("gg-eye")).addClass("reveal-button");
+        button.on("click", fn)
+        $(el).children(":not(.solution-label)").addClass("blur").on("click", fn);
+        $(el).children().first().after(button);
+    });
+
+    $(".auction.revealable").each( function(index, el) {
+
+        let controls = $("<div>").addClass("reveal-controls");
+        let bids = $(el).children(".auction-bid");
+        bids.hide();
+        let curr = bids.first();
+
+        let b1 = $("<button>").text(">|").addClass("reveal-control").on("click", function() {
+            curr.show();
+            curr = curr.next(".auction-bid");
+            if(!curr.length) {
+                controls.hide();
+            }
+        });
+        let ba = $("<button>").text(">>|").addClass("reveal-control").on("click", function() {
+            bids.show();
+            controls.hide();
+        });
+
+        controls.append(b1, ba);
+
+        $(el).after(controls);
+    });
+
 });
